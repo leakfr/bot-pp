afc062d83506190162e2fd8950e76eca5884b935
LeakFR <90473549+leakfr@users.noreply.github.com>
2021-09-10 22:16:22 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+  const Discord = require('discord.js')
+const {MessageEmbed} = require('discord.js')
+const client  = new Discord.Client()
+const config = require('./config.json')
+client.on('ready', async () => {
+setInterval(() => {
+  let guild = client.guilds.cache.get(config.guild)
+  if(!guild) return console.log('Guild introuvable')
+    let channel = guild.channels.cache.get(config.salon)
+    if(!channel) return console.log('Salon introuvable')
+    if(channel) {  
+const user = client.users.cache.random()
+if(user.bot) return;
+const embed = new Discord.MessageEmbed()
+.setImage(user.displayAvatarURL({ dynamic : true  }))
+.setColor(`#2f3136`)
+
+channel.send(embed);
+}
+
+
+}, 6000)})
+client.login(token)
